79bc1a17397d3dc06276889c01631a8881b95de9
Stephen Goss <stephengoss@gmx.net>
2022-10-16 11:47:52 +0200

    deleted:    start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-echo "# BlinkJulian" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/stephengoss/BlinkJulian.git
-git push -u origin main
